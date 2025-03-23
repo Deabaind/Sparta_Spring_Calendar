d2b1d1773911e1645369a583ee467739065024af
deabaind <deabaind@gmail.com>
2025-03-24 01:20:57 +0900

    Lv0 수정, 회고 작성
    Lv0
     SQL
      변경된 칼럼 수정
    
    회고 작성 1
    https://deabaind.tistory.com/33

--- schedule.sql
+++ schedule.sql
@@ -1,18 +1,18 @@
-create table Calendar (
-    Calendar_ID int primary key,
-    Title varchar(150),
-    Contents varchar(255),
-    Start_Date timestamp,
-    Last_Date timestamp,
-    User_ID int,
-    foreign key (User_ID) references USer(User_ID)
+create table schedule (
+    schedule_ID int primary key,
+    user_ID int,
+    title varchar(50),
+    contents varchar(255),
+    start_Date timestamp,
+    last_Date timestamp,
+    update_Date timestamp,
+    foreign key (user_ID) references USer(user_ID)
 );
 
 create table User (
-    User_ID int PRIMARY KEY,
-    First_Name varchar(100),
-    Last_Name varchar(100),
-    Email varchar(100),
-    Calendar_ID int,
-    FOREIGN KEY (Calendar_ID) references Calendar(Calendar_ID)
+    user_ID int PRIMARY KEY,
+    schedule_ID int,
+    name varchar(50),
+    password varchar(50),
+    FOREIGN KEY (schedule_ID) references schedule(schedule_ID)
 );
